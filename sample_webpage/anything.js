b218866480e18fe97d5add9effc7fefbf2d63ec3
Vinzenz Sauer <vinzenz.sauer@gmx.at>
2020-02-10 13:54:30 +0100

    final_2

--- sample_webpage/anything.js
+++ sample_webpage/anything.js
@@ -24,28 +24,3 @@ $(".sidemenu").append("<li><a href=\"#Contact Us\"><span>Contact Us</span></a></
 $("#sidebar p:first").text("When someone says: 'I want a programming language in which I need only to say what I wish, and done', give him a lollipop.");
 
 
-// $(function(){
-//     $('a').attr('href', (n, old) => 'https://codefactory.wien' + old);
-// });
-
-// $(function(){
-// $(code).css('color', 'blue');
-// });
-
-// $(function(){
-// $("ol li").css("background-color", "yellow");
-// });
-
-// $("img").attr("src","https://www.codefactory.wien/wp-content/uploads/2018/03/codefactory-vienna-footer-h.jpg");
-
-// // $(document).ready(function(){ 
-// //   $('form').find("input[type=textarea], input[type=password], textarea").each(function(ev)
-// //   {
-// //       if(!$(this).val()) { 
-// //      $(this).attr("placeholder", "Express your opinion");
-// //   }
-
-
-// //   });
-// // });
-
